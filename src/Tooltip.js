dbddd0ac4bdf4873b299c0bc09cda0446a128e56
Krishh258 <krishnanshu33@gmail.com>
2020-08-09 18:14:19 +0530

    added full functionality

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -3,6 +3,41 @@ import React from 'react';
 
 class Tooltip extends React.Component{
 
+    constructor(){
+        super();
+        this.state = {
+            position: "displayNoneClass"
+        }
+    }
+
+    setPositionLeft = () => {
+        this.setState({
+          position : "left"
+        });
+    }
+
+    setPositionTop = () => {
+        this.setState({
+          position : "top"
+        });
+    }
+
+    setPositionRight = () => {
+        this.setState({
+          position : "right"
+        });
+    }
+
+    setPositionBottom = () => {
+        this.setState({
+          position : "bottom"
+        });
+    }
+
+
+
+
+
     render(){
         return(
          <div className="tooltip-container">
@@ -11,25 +46,25 @@ class Tooltip extends React.Component{
             </div>
             <div className="input-container">   
              <div>
-             <input type="radio" name="position" id="left" />
+             <input type="radio" name="position" id="left" onChange={this.setPositionLeft}/>
              <label for="left">Left</label>
              </div> 
              <div>
-             <input type="radio" name="position" id="top" />
+             <input type="radio" name="position" id="top" onChange={this.setPositionTop}/>
             <label for="top">Top</label>   
             </div>  
             <div>
-            <input type="radio" name="position" id="right" />
+            <input type="radio" name="position" id="right" onChange={this.setPositionRight}/>
             <label for="right">Right</label>    
             </div>  
             <div>
-            <input type="radio" name="position" id="bottom" />
+            <input type="radio" name="position" id="bottom" onChange={this.setPositionBottom}/>
             <label for="bottom">Bottom</label>    
             </div>  
             </div>
             <div className="tooltip-main-container">
                <button>Hover Over Me</button>
-               <div className="tooltip-text bottom">
+               <div className= {this.state.position}>
                 <p>
                     Hello There!
                 </p>
